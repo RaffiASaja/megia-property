2c5eb4bddb76a576d6ada0eaa20a5d919e654352
RaffiAS <firasmontis0@gmail.com>
2026-05-11 18:34:14 +0800

    mindahin file fix

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ define('LARAVEL_START', microtime(true));
 
 // Determine if the application is in maintenance mode...
 // Path diubah: dari __DIR__.'/../storage...' menjadi __DIR__.'/storage...'
-if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
+if (file_exists($maintenance = __DIR__.'/storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
 // Register the Composer autoloader...
 // Path diubah: dari __DIR__.'/../vendor...' menjadi __DIR__.'/vendor...'
-require __DIR__.'/../vendor/autoload.php';
+require __DIR__.'/vendor/autoload.php';
 
 // Bootstrap Laravel and handle the request...
 /** @var Application $app */
 // Path diubah: dari __DIR__.'/../bootstrap...' menjadi __DIR__.'/bootstrap...'
-$app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
 
 $app->handleRequest(Request::capture());
